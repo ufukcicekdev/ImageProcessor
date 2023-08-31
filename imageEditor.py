242392868a8287aaa6c8ab2f7356d5ae475de0ca
EAGLEEARTH <ufukcicek199@gmail.com>
2023-08-31 23:30:04 +0300

    changes project

--- imageEditor.py
+++ imageEditor.py
@@ -1,16 +1,27 @@
-from PIL import Image, ImageEnhance, ImageFilter
-import os
+from PIL import Image,ImageChops,ImageColor
 
 
+def image_diff(file_img_1,file_img_2):
 
-path = './imgs'
-pathOut = './editedImgs'
+    img1 = Image.open(file_img_1).convert('RGB')
+    img2 = Image.open(file_img_2).convert('RGB')
+    diff = ImageChops.difference(img1, img2)
 
-for filename in os.listdir(path):
-    img = Image.open(f"{path}/{filename}")
-    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(+90)
-    factor = 1.5
-    enhancer = ImageEnhance.Contrast(edit)
-    edit = enhancer.enhance(factor)
-    clean_name = os.path.splitext(filename)[0]
-    edit.save(f"{pathOut}/{clean_name}_edited.jpg")
+    RED = ('red')
+    RL = Image.new('RGB', diff.size, RED) # Make a red layer the same size
+    RedDiff = ImageChops.multiply(RL, diff)
+    Result = ImageChops.blend(RedDiff, img1, alpha=0.07)
+
+    datas = Result.getdata()
+    newData = []
+    for items in datas:
+        if items[0] == 255 and items[1] == 255 and items[2] == 255:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(items)
+    Result.putdata(newData)
+    Result.putalpha(128)
+    img1.paste(Result,mask=Result)
+    img1.save("dif.png",quality=95)
+
+image_diff('img1.PNG','img2.PNG')
